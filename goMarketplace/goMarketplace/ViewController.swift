c727b4e714e7f70915f41c5ecd423fc225d9d977
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-01 14:59:35 -0300

    feat: insert assest and inicial ViewController

---
+++ goMarketplace/goMarketplace/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  goMarketplace
+//
+//  Created by Grazielli Berti on 01/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
